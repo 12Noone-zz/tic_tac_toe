4096bd488bb02b0399e358c87d85506855b11543
Jill Noone <noone1200@gmail.com>
2015-06-07 19:59:18 -0400

    tic tac toe

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,16 +1,16 @@
 var playerOneObject = {
 	Name: "player",
 	piece: "X",
-	wins: 0
+	wins: 0,
+	$scoreBoard: $('#p1score')
 }
 
 var playerTwoObject = {
 	Name: "playertwo",
 	piece: "O",
-	wins: 0
+	wins: 0,
+	$scoreBoard: $('#p2score')
 }
-
-var $body = $('<body>');
 
 var $startButton = $('#start-button');
 $startButton.on('click', function() {
@@ -19,47 +19,48 @@ $startButton.on('click', function() {
 	var playerTwo = prompt("Enter your name, Player Two");
 	playerTwoObject.Name = playerTwo;
 
-	var $player1View = $('#player-one').addClass('.piece-style').text(playerOneObject.Name);
-	var $player2View = $('#player-two').addClass('.piece-style').text(playerTwoObject.Name);
+	var $player1View = $('#player-one').text(playerOneObject.Name);
+	var $player2View = $('#player-two').text(playerTwoObject.Name);
 
 });
 
 var ticTacToeArray = [[null, null, null], 
 
-				  	[null, null, null],    
+				  	 [null, null, null],    
 				  
-				  	[null, null, null]];   
+				  	 [null, null, null]];  
 
-var $rowOne = $('.row1');
-var $rowTwo = $('.row2');
-var $rowThree = $('.row3');
+var defaultTicTacToeArray = [[null, null, null], 
 
+				  			[null, null, null],    
+				  
+				  			[null, null, null]];   
 
 var $gameBoard = $('.tic-tac-toe');
-var turn = playerOneObject;
+var $player1View = $('#player-one');
+var $player2View = $('#player-two');
 
 $gameBoard.on('click', 'td', function(eventObject) {
 	var $currentSquare = $(this);
 	var cellIndex = $currentSquare.index(); 
 	var rowIndex = $currentSquare.parents('tr').index();
 	if (ticTacToeArray[rowIndex][cellIndex] === null) {
-		$currentSquare.text(turn.piece).addClass('piece-style');
+		$currentSquare.text(turn.piece);
 		ticTacToeArray[rowIndex][cellIndex] = turn;
-		console.log(ticTacToeArray[rowIndex][cellIndex]);
 		determineWinner();
 		changePlayer();
-
 	}
 	else {
 		return;
 	}
 });
 
-
+var turn = playerOneObject;
 
 var changePlayer = function() {
 	if (turn == playerOneObject) {
-		turn = playerTwoObject;	
+		turn = playerTwoObject;
+			
 	}
 	else {
 		turn = playerOneObject;
@@ -68,12 +69,12 @@ var changePlayer = function() {
 }
 
 var determineWinner = function() {
-	var $p1h4 = $('#p1score');
-	var $p2h4 = $('#p2score');
 	var count = 0;
 	var winner = false;
 	if ((ticTacToeArray[0][0] === turn) && (ticTacToeArray[1][1] === turn) && (ticTacToeArray[2][2] === turn)) {
 			alert(turn.Name + " wins on the diagonal!");
+			
+
 		}
 		else if ((ticTacToeArray[0][2] === turn) && (ticTacToeArray[1][1] === turn) && (ticTacToeArray[2][0] === turn)) {
 			alert(turn.Name + " wins on the diagonal!");
@@ -102,9 +103,28 @@ var determineWinner = function() {
 	}
 	if (winner) {
 		alert(turn.Name + " is the winner!");
+		turn.wins = turn.wins + 1;
+		turn.$scoreBoard.text(turn.wins);
 	}
-	if (turn === playerOneObject) {
-		playerOneObject.win += 1;
-		$p1h4.text(playerOneObject.wins);
 }
-}
+
+	var $resetButton = $('#reset-button');
+	$resetButton.on('click', function() {
+		var $player1Score = $('#p1score');
+		var $player2Score = $('#p2score');
+		ticTacToeArray = defaultTicTacToeArray;
+		$gameBoard.find('td').text('');
+		$player1View.text('');
+		$player2View.text('');
+		$player1Score.text('');
+		$player2Score.text('');
+	});
+
+	var $newGame = $('#new-game');
+	$newGame.on('click', function() {
+		ticTacToeArray = defaultTicTacToeArray;
+		$gameBoard.find('td').text('');
+	})
+
+
+
